e43ec5eb40440b75bf21c346f9a63eef619319e6
Ojas Shirekar <ojas.shirekar@gmail.com>
2017-04-10 15:58:03 +0530

    can now boot into long mode

--- Makefile
+++ Makefile
@@ -12,8 +12,12 @@ build/boot.o: boot.asm
 	mkdir -p build
 	nasm -f elf64 boot.asm -o build/boot.o
 
-build/kernel.bin: build/multiboot_header.o build/boot.o linker.ld
-	x86_64-pc-elf-ld -n -o build/kernel.bin -T linker.ld build/multiboot_header.o build/boot.o
+build/long_mode_start.o: long_mode_start.asm
+	mkdir -p build
+	nasm -f elf64 long_mode_start.asm -o build/long_mode_start.o
+
+build/kernel.bin: build/multiboot_header.o build/boot.o build/long_mode_start.o linker.ld
+	x86_64-pc-elf-ld -n -o build/kernel.bin -T linker.ld build/multiboot_header.o build/boot.o build/long_mode_start.o
 
 build/os.iso: build/kernel.bin grub.cfg
 	mkdir -p build/isofiles/boot/grub
--- boot.asm
+++ boot.asm
@@ -1,4 +1,5 @@
 global start
+extern long_mode_start
 
 section .text
 bits 32
@@ -9,7 +10,13 @@ start:
     call check_cpuid
     call check_long_mode
 
-    
+    call set_up_page_tables
+    call enable_paging
+
+    ; load the 64 bit gdt64
+    lgdt [gdt64.pointer]
+
+    jmp gdt64.code:long_mode_start
 
     mov word [0xb8000], 0x0248 ; H
     mov word [0xb8002], 0x0265 ; e
@@ -24,7 +31,7 @@ start:
     mov word [0xb8014], 0x026c ; l
     mov word [0xb8016], 0x0264 ; d
     mov word [0xb8018], 0x0221 ; !
-    mov word [0xb8020], 0xfff
+    mov word [0xb8020], 0x00000000 ;NULL bytes
     hlt
 
 error:
@@ -95,7 +102,74 @@ check_long_mode:
     mov al, "2"
     jmp error
 
+set_up_page_tables:
+    ; map first p4 entry to p3 table
+    mov eax, p3_table
+    or eax, 0b11 ;present + writeable
+    mov [p4_table], eax
+
+    ;map first entry of p3 to p2_table
+    mov eax, p2_table
+    or eax, 0b11
+    mov[p3_table], eax
+
+    ; map each P2 entry to a huge 2MiB page
+    mov ecx, 0
+.map_p2_table:
+    ; map ecx-th P2 entry to a huge page that starts at address 2MiB*ecx
+    mov eax, 0x200000   ;2MiB
+    mul ecx             ; start address of ecx-th page
+    or eax, 0b10000011  ;present + writeable + huge
+    mov [p2_table + ecx * 8], eax ; map ecx-th entry
+    inc ecx                     ; increment ecx counter
+    cmp ecx, 512                ; if counter == 512 whole table is mapped
+    jne .map_p2_table           ; else map next entry
+
+    ret
+
+enable_paging:
+    ; load P4 to cr3 register (cpu uses this to access the P4 table)
+    mov eax, p4_table
+    mov cr3, eax
+
+    ; enable PAE-flag in cr4 (Physical Address Extension)
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ; set the long mode bit in the EFER MSR (model specific register)
+    mov ecx, 0xc0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    ; enable paging in cr0 register
+    mov eax, cr0
+    or eax, 1 << 31
+    or eax, 1 << 16
+    mov cr0, eax
+
+    ret
+
 section .bss
+align 4096
+
+p4_table:
+    resb 4096
+p3_table:
+    resb 4096
+p2_table:
+    resb 4096
+
 stack_bottom:
     resb 64
 stack_top:
+
+section .rodata
+gdt64:
+    dq 0 ; zero entry
+.code: equ $ - gdt64
+    dq (1<<43) | (1<<44) | (1<<47) | (1<<53) ; code segment
+.pointer:
+    dw $ - gdt64 - 1
+    dq gdt64
